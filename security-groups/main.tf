59ce3579e33a454465bd9e4bfe3fc559235556e1
Menna114 <mennadiaah14@gmail.com>
2025-03-23 07:09:34 +0200

    creating security group for jenkins to allow port 8080

--- security-groups/main.tf
+++ security-groups/main.tf
@@ -1,5 +1,6 @@
 variable "ec2_sg_name" {}
 variable "vpc_id" {}
+variable "ec2_jenkins_sg_name" {}
 
 resource "aws_security_group" "ec2_sg_ssh_http" {
   name        = var.ec2_sg_name
@@ -31,6 +32,24 @@ resource "aws_security_group" "ec2_sg_ssh_http" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Security Groups to allow SSH(22) and HTTP(80)"
+  }
+}
+resource "aws_security_group" "ec2_jenkins_port_8080" {
+  name        = var.ec2_jenkins_sg_name
+  description = "Enable the Port 8080 for jenkins"
+  vpc_id      = var.vpc_id
+
+  # ssh for terraform remote exec
+  ingress {
+    description = "Allow 8080 port to access jenkins"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
   }
 
   tags = {
