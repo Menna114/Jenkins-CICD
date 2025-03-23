595238af6c0dcda81ddf579684749510d8341d56
Menna114 <mennadiaah14@gmail.com>
2025-03-23 06:27:47 +0200

    created internet gateway for my public subnets

--- networking/main.tf
+++ networking/main.tf
@@ -34,3 +34,11 @@ resource "aws_subnet" "dev_proj_1_private_subnets" {
     Name = "dev-proj-private-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "dev_proj_1_public_internet_gateway" {
+  vpc_id = aws_vpc.dev_proj_1_vpc.id
+  tags = {
+    Name = "dev-proj-1-igw"
+  }
+}
+
