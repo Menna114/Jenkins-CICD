2fcf1c4acaaaff67dbd5a7d26ec5eb89a836c4bf
Menna114 <mennadiaah14@gmail.com>
2025-03-23 06:23:22 +0200

    some modifications

--- networking/main.tf
+++ networking/main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "dev_proj_1_public_subnets" {
 # private subnet
 resource "aws_subnet" "dev_proj_1_private_subnets" {
   count             = length(var.cidr_private_subnet)
-  vpc_id            = aws_vpc.dev_proj_1_vpc_eu_central_1.id
+  vpc_id            = aws_vpc.dev_proj_1_vpc.id
   cidr_block        = element(var.cidr_private_subnet, count.index)
   availability_zone = element(var.eu_availability_zone, count.index)
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-vpc_cider            = "11.0.0.0/16"
+vpc_cidr            = "11.0.0.0/16"
 vpc_name             = "dev-proj-1-vpc-1"
 cidr_public_subnet   = ["11.0.1.0/24", "11.0.2.0/24"]
 cidr_private_subnet  = ["11.0.3.0/24", "11.0.4.0/24"]
